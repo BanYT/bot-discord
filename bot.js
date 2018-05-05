ca519b9c832a34d965d336c6b3b41c86480f91ae
lucasdavid64536 <38979101+lucasdavid64536@users.noreply.github.com>
2018-05-05 08:16:48 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -11,7 +11,7 @@ client.on('message', message => {
   	}
     client.on('message', message => {
     if (message.content === 'lhelp') {
-    	message.reply('---Comenzile mele sunt--
+    	message.reply('Comenzile mele sunt
                       lhelp - comenzile
                       linfo - despre mine
                       in curand '  );
