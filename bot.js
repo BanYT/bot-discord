36a8e86413454cf383d88dd01d209b5f07aa2033
lucasdavid64536 <38979101+lucasdavid64536@users.noreply.github.com>
2018-05-06 21:02:22 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -8,15 +8,7 @@ Client.on("ready", () => {
 });
 
 
-Client.on("message", async (message) => {
-	if (message.author.bot) return;
-	if (!message.content.startsWith(prefix)) return;
-	
-	let command = message.content.split(" ")[0];
-	command = command.slice(prefix.length);
-	
-	let args = message.content.split(" ").slice(1);
-	
+Client.on("message", async (message) => {	
 	if (command === "ping") {
 		message.channel.send(`Pong! Time took: ${Date.now() - message.createdTimestamp} ms`);
 	} else
