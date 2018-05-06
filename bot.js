35251b745e542738f1965011c7af63402828bd77
lucasdavid64536 <38979101+lucasdavid64536@users.noreply.github.com>
2018-05-06 21:15:47 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -9,54 +9,7 @@ client.on('message', message => {
     if (message.content === 'ping') {
     	message.reply('pong');
   	
-    } else
-
-	if (command === "say") {
-		message.delete()
-                const embed = new Discord.RichEmbed()
-		.setColor(0x954D23)
-		.setDescription(message.author.username + " says: " + args.join(" "));
-		message.channel.send({embed})
-	} else
-
-   if (command === "cat") {
-	   const { body } = await superagent
-	   .get('aws.random.cat/meow');
-	   const embed = new Discord.RichEmbed()
-	   .setColor(0x954D23)
-	   .setTitle("Meow :cat:")
-	   .setImage(body.file)
-	   message.channel.send({embed})
-   } else
-
-   if (command === "announcement") {
-	   if (message.member.hasPermission("ADMINISTRATOR")) {
-		   const color = args[0]
-				
-		   const text = args.slice(1).join(" ");
-		   if (text.length < 1) return message.channel.send("Can not announce nothing");
-		   //const colour = args.slice(2).join("");
-		   const embed = new Discord.RichEmbed()
-		   .setColor("0x" + color)
-		   .setTitle("Important Announcement:")
-		   .setDescription(text);
-		   message.channel.send("@everyone")
-		   message.channel.send({embed})
-	   }
-   } else
-
-	if (command == "help") {
-		const embed = new Discord.RichEmbed()
-		.setColor(0x954D23)
-		.setTitle("Command List:")
-		.addField("!help", "Will give the current command list")
-		.addField("!ping", "WIll show the ping time for the bot")
-		.addField("!say [text]", "Will make the bot say something")
-		.addField("!announcement [text]", "Will make the bot say an announcement and tag everyone")
-		.addField("!cat", "Will send a random cat image");
-		message.channel.send({embed})
-	}
-
+    } 
 });
 
 Client.login("BOT_TOKEN"); 
