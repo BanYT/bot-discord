69d6f60c48d60e852a58b01fc202d3076117075d
lucasdavid64536 <38979101+lucasdavid64536@users.noreply.github.com>
2018-05-24 07:30:42 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,8 +1,19 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
 
-client.on('ready', () => {
-    console.log('dnd');
+client.on('guildMemberAdd', member => {
+    let channel = member.guild.channels.find('name', 'chatbox');
+    let memberavatar = member.user.avatarURL
+        if (!channel) return;
+        let embed = new Discord.RichEmbed()
+        .setColor('#FF000')
+        .setThumbnail(memberavatar)
+        .addField('✘ | Nume: ', ${member})
+        .addField('✘ | Bun Venit !', Bun Venit Pe Server Sedare Placuta, ${member})
+        .setTimestamp()
+
+        channel.sendEmbed(embed);
+});
 });
 
 client.on('message', message => {
