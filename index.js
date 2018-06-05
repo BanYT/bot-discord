5cfdc5ba62785a839acb3b8ee39adef4a695d829
lucasdavid64536 <38979101+lucasdavid64536@users.noreply.github.com>
2018-06-05 21:26:49 +0300

    Update index.js

--- index.js
+++ index.js
@@ -6,6 +6,20 @@ const bot = new Discord.Client({disableEveryone: true})
 bot.on("ready", async () => {
   console.log(`${bot.user.username} is online!`);
   bot.user.setGame("stargang.ml | /help| PayPal.Me/Roarmice");
-});
+  });
+  client.on('message', message => {    
+    if(message.content.startsWith('/mass')) {    
+    if(message.author.id === "419472407816830986" ||
+message.author.id === "419472407816830986"){        
+       let args = message.content.split(" ").slice(1);         
+       var argresult = args.join(" ")      
+const argsresult = args.join(" ")         
+       let reason = args.join(" ")                   
+    if(!args[1]) {  }  
+    if(args[1]) { 
+message.channel.guild.members.forEach(member => {{ 
+member.send(reason)
+message.delete() }})}}} });
+
 
 bot.login(botconfig.token);
