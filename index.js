25d15995c3de847d45445f0c212a3e61bd6dacc4
lucasdavid64536 <38979101+lucasdavid64536@users.noreply.github.com>
2018-06-14 12:50:09 +0300

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const Discord = require('discord.js');
 const client = new Discord.Client();
 
 client.on('ready', () => {
-  client.user.setGame('play', 'https://www.twitch.tv/lucasdavid913/')
+  client.user.setGame(' https://discord.gg/hXJZhqa ', 'https://www.twitch.tv/lucasdavid913/')
 })   
 
 client.on('message', message =>{
