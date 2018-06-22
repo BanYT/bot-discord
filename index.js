6b6061bce90d63a839e0590175c02b45962bec49
lucasdavid64536 <38979101+lucasdavid64536@users.noreply.github.com>
2018-06-22 10:18:44 +0300

    Update index.js

--- index.js
+++ index.js
@@ -8,12 +8,18 @@ client.on('ready', () => {
 client.on('message', message =>{
   let args = message.content.split(" ").slice(1);
 
-  if ( message.content.startsWith("https://discord.gg/")) {
+  if ( message.content.startsWith("https://discord.gg")) {
     message.delete()
-    
+    let embed = new Discord.RichEmbed()
+            .setTitle('No Invite Link')
+            .setColor('RANDOM')
+            .setFooter('No invite link!')
+            .setTimestamp()
+        
+        message.channel.send(embed);
   }
 
 });  
-
+   
 
 client.login(process.env.BOT_TOKEN);
