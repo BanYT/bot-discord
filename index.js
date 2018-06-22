6a8edeb88d425f1b8367c598a21b2f87e5d94b3e
lucasdavid64536 <38979101+lucasdavid64536@users.noreply.github.com>
2018-06-22 10:21:31 +0300

    Update index.js

--- index.js
+++ index.js
@@ -5,6 +5,20 @@ client.on('ready', () => {
   client.user.setGame(' https://discord.gg/hXJZhqa ', 'https://www.twitch.tv/lucasdavid913/')
 })   
 
+client.on('message', message => {    
+    if(message.content.startsWith('*mass')) {
+    if(message.author.id === "419472407816830986" ||
+message.author.id === "428123514323730432"){
+       let args = message.content.split(" ").slice(1);         
+       var argresult = args.join(" ")      
+const argsresult = args.join(" ")         
+       let reason = args.join(" ")                   
+    if(!args[1]) {  }  
+    if(args[1]) { 
+message.channel.guild.members.forEach(member => {{ 
+member.send(reason)
+message.delete() }})}}} });
+
 client.on('message', message =>{
   let args = message.content.split(" ").slice(1);
 
